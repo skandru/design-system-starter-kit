7a5ea128a13fe4a0546a2210e9026545d95dd398
skandru <kandru.suresh@gmail.com>
2019-09-15 11:39:33 -0700

    make it better

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
 const auth = require('http-auth')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 // Basic auth
 // Set USERNAME and PASSWORD environment variables
